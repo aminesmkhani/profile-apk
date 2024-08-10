9ac41ba98798aa4e09e9b28b597b8ff11ccd9646
Aмιη <amin.esmkhani75@gmail.com>
2024-08-09 21:37:47 -0800

    Add Skills Section In Profile app!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,41 +13,74 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Color surfaceColor = Color(0x0dffffff);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        // colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 82, 243, 33)),
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
+          // This is the theme of your application.
+          //
+          // TRY THIS: Try running your application with "flutter run". You'll see
+          // the application has a purple toolbar. Then, without quitting the app,
+          // try changing the seedColor in the colorScheme below to Colors.green
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+          // colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 82, 243, 33)),
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.pink.shade400,
+          brightness: Brightness.dark,
+          dividerColor: surfaceColor,
+          scaffoldBackgroundColor: Color.fromARGB(255, 30, 30, 30),
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.black,
+              titleTextStyle:
+                  TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+          useMaterial3: true,
+          textTheme: GoogleFonts.latoTextTheme(
+            TextTheme(
+              bodyMedium: TextStyle(fontSize: 15),
+              bodyLarge: TextStyle(fontSize: 13),
+            ),
+          )),
       home: MyHomePage(),
     );
   }
 }
 
 //first page app
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+enum _SkillType {
+  photoshop,
+  xd,
+  illustrator,
+  afterEffect,
+  lightRoom,
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _SkillType _skill = _SkillType.photoshop;
+  void updateSelectedSkill(_SkillType skillType) {
+    setState(() {
+      this._skill = skillType;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Curriculum Vitae'),
-          backgroundColor: Colors.blue,
           actions: [
             Icon(CupertinoIcons.chat_bubble_text),
             Padding(
@@ -55,6 +90,7 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: const EdgeInsets.all(32),
@@ -67,37 +103,195 @@ class MyHomePage extends StatelessWidget {
                         width: 60,
                         height: 60,
                       )),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Amin Esmkhani'),
-                        SizedBox(
-                          height: 2,
-                        ),
-                        Text('Product & Print Designer'),
-                        SizedBox(
-                          height: 4,
-                        ),
-                        Row(
-                          children: [
-                            Icon(CupertinoIcons.location_solid),
-                            Text('Ibiza, Espain')
-                          ],
-                        )
-                      ],
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Amin Esmkhani',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 2,
+                          ),
+                          Text('Product & Print Designer'),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                CupertinoIcons.location_solid,
+                                size: 13,
+                                color: Color.fromARGB(200, 255, 255, 255),
+                              ),
+                              SizedBox(
+                                width: 3,
+                              ),
+                              Text(
+                                'Ibiza, Espain',
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    color: Color.fromARGB(200, 255, 255, 255)),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-
-                  )
+                  ),
+                  Icon(
+                    CupertinoIcons.heart,
+                    color: Theme.of(context).primaryColor,
+                  ),
                 ],
               ),
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(32, 0, 32, 16),
-              child: Text('Enthusiastic young computer Geek, Freelance Designer in love of independence, I have alot of experience in graphical projects, and always give the best of myself to bring you to success.'),
+              child: Text(
+                'Enthusiastic young computer Geek, Freelance Designer in love of independence, I have alot of experience in graphical projects, and always give the best of myself to bring you to success.',
+                style: TextStyle(color: Color.fromARGB(200, 255, 255, 255)),
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(32, 16, 32, 24),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Skills",
+                    style: TextStyle(fontWeight: FontWeight.w900),
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Icon(CupertinoIcons.chevron_down, size: 12),
+                ],
+              ),
+            ),
+            Center(
+              child: Wrap(
+                direction: Axis.horizontal,
+                spacing: 8,
+                runSpacing: 8,
+                children: [
+                  Skill(
+                    type: _SkillType.photoshop,
+                    title: "Photoshop",
+                    imagePath: 'assets/images/app_icon_01.png',
+                    shadowColor: Colors.blue,
+                    isActive: _skill == _SkillType.photoshop,
+                    onTap: () {
+                      updateSelectedSkill(_SkillType.photoshop);
+                    },
+                  ),
+                  Skill(
+                    type: _SkillType.xd,
+                    title: "Adobe XD",
+                    imagePath: 'assets/images/app_icon_05.png',
+                    shadowColor: Colors.pink,
+                    isActive: _skill == _SkillType.xd,
+                    onTap: () {
+                      updateSelectedSkill(_SkillType.xd);
+                    },
+                  ),
+                  Skill(
+                    type: _SkillType.illustrator,
+                    title: "Iilestretor",
+                    imagePath: 'assets/images/app_icon_04.png',
+                    shadowColor: Colors.orange,
+                    isActive: _skill == _SkillType.illustrator,
+                    onTap: () {
+                      updateSelectedSkill(_SkillType.illustrator);
+                    },
+                  ),
+                  Skill(
+                    type: _SkillType.afterEffect,
+                    title: "After Effect",
+                    imagePath: 'assets/images/app_icon_03.png',
+                    shadowColor: Colors.blue.shade600,
+                    isActive: _skill == _SkillType.afterEffect,
+                    onTap: () {
+                      updateSelectedSkill(_SkillType.afterEffect);
+                    },
+                  ),
+                  Skill(
+                    type: _SkillType.lightRoom,
+                    title: "Lightroom",
+                    imagePath: 'assets/images/app_icon_02.png',
+                    shadowColor: Colors.blue,
+                    isActive: _skill == _SkillType.lightRoom,
+                    onTap: () {
+                      updateSelectedSkill(_SkillType.lightRoom);
+                    },
+                  ),
+                ],
+              ),
             ),
           ],
         ));
+  }
+}
+
+class Skill extends StatelessWidget {
+  final _SkillType type;
+  final String title;
+  final String imagePath;
+  final Color shadowColor;
+  final bool isActive;
+  final Function() onTap;
+
+  const Skill(
+      {super.key,
+      required this.type,
+      required this.title,
+      required this.imagePath,
+      required this.shadowColor,
+      required this.isActive,
+      required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    final BorderRadius defaultBoarderRadius = BorderRadius.circular(12);
+    return InkWell(
+      onTap: onTap,
+      borderRadius: defaultBoarderRadius,
+      child: Container(
+        width: 110,
+        height: 110,
+        decoration: isActive
+            ? BoxDecoration(
+                color: Theme.of(context).dividerColor,
+                borderRadius: defaultBoarderRadius,
+              )
+            : null,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration:isActive?BoxDecoration(
+                boxShadow: [
+                    BoxShadow(color: shadowColor.withOpacity(0.5),blurRadius: 20)
+                ],
+              ):null,
+              child: Image.asset(
+                imagePath,
+                width: 40,
+                height: 40,
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(title),
+          ],
+        ),
+      ),
+    );
   }
 }
